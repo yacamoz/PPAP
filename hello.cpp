8a40b4b061de920da759df0cd0ba470254cfc616
Shin_BG <good03447@gmail.com>
2021-08-11 10:32:56 +0900

    변수정의

---
+++ hello.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int num1;
+    int num2;
+    int num3;
+
+    num1 = 10;
+    num2 = 5;
+    num3 = 1;
+    
+    printf("%d, %d, %d", num1, num2, num3);
+
+    printf("\nhello world!");
+    return 0;
+}    
