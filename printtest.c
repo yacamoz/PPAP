4fded3d2ee10a811f9f6b9941f9f30ffa1df404f
yacamoz <llbk123456ll@gmail.com>
2021-08-11 13:27:50 +0900

    프린트과제+개인적인HTML

---
+++ printtest.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    printf("HongGilDong\nHong Gil Dong\nHong  Gil  Dong\n");
+    // Korean doesn't work
+    printf("my name is 손병규");
+    printf("\nMy phone number: %s-%d-%d", "010", 27273, 3177);
+    printf("\nMy home address: GUMI, Song Dong Lo %d", 128);
+
+    return 0;
+}
