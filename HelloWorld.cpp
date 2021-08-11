6a335d9208880594e6c0aa7f0a0af189896dd462
yacamoz <llbk123456ll@gmail.com>
2021-08-11 11:28:03 +0900

    이프 넣어보기

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,6 +1,12 @@
 #include <stdio.h>
 int main()
 {
-    printf("HelloWorld");
+    int a = 4;
+    int b = 2;
+    int c = 3;
+    if (a*c > 10)
+        printf("%d %d %d\n", a+b, b*c, c);
+    else if (a*c < 10)
+        printf("GURA\n");
     return 0;
 }
