2f63d410083b5419827274677929c0c25e392504
yacamoz <llbk123456ll@gmail.com>
2021-08-12 15:38:58 +0900

    로그 지우기

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,12 +1,12 @@
 #include <stdio.h>
 int main()
 {
-    int a = 4;
+    int age = 4;
     int b = 2;
     int c = 3;
-    if (a*c > 10)
-        printf("%d %d %d\n", a+b, b*c, c);
-    else if (a*c < 10)
+    if (b*c > 10)
+        printf("%d %d %d\n", c+b, b*c, c);
+    else if (b*c < 10)
         printf("GURA\n");
     return 0;
 }
